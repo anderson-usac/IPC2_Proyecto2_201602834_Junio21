91ea14eb77dfaaf0d15e412eee457a2ab8ff20db
anderson-usac <2976112160101@ingenieria.usac.edu.gt>
2021-06-28 21:28:49 -0600

    probando regex

--- Proyecto2.py
+++ Proyecto2.py
@@ -1,5 +1,10 @@
-from flask import Flask,request,Response
+from typing import IO
+from flask import Flask,request,Response,make_response
 from flask_cors import CORS
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+from io import StringIO
+import io
 
 
 app=Flask(__name__)
@@ -16,13 +21,13 @@ def GET_datos():
     files=open('prueba.txt')
     
     return Response(status=200,response=files.read(),content_type='text/plain')
-
 @app.route('/datos',methods=['POST'])
 def POST_datos():
     file=request.data.decode('utf-8')
     files=open('prueba.txt','w')
     files.write(file)
     files.close()
-    return Response(status=204)
+    return Response(status=200)
+
 if __name__=="__main__":
     app.run(debug=True)
--- frontend/web/views.py
+++ frontend/web/views.py
@@ -1,18 +1,32 @@
 #from django.conf.urls import url
 from django.shortcuts import render,redirect
 import requests
+import csv
+import re
 
 endpoint= 'http://localhost:5000/{}'
 # Create your views here.
 def index(request):
     if request.method=='GET':
         url=endpoint.format('/datos')
-        data=requests.get(url)
-        context={'data':data.text,}
+        doccsv=requests.get(url)
+        context={'data':doccsv.text,}
         return render(request,'index.html',context)
     elif request.method=='POST':
-        doc=request.FILES['document']
-        data=doc.read()
+        doccsv=request.FILES['document'].read()
+        datacsv=csv.DictReader(doccsv)
+        for f in datacsv:
+            nombre=f.get('Nombre')
+            nom=re.fullmatch('[a-zA-ZáéíóúñÑ]',nombre)
+            apellidos=f.get('Aperllidos')
+            ape=re.fullmatch('[a-zA-ZáéíóúñÑ]',apellidos)
+            edad=f.get('Edad')
+            ed=re.fullmatch('[0-9]',edad)
+            fechanac=f.get('Fecha de cumpleaños')
+            fnac=re.fullmatch('^(?:3[01]|[12][0-9]|0?[1-9])([\/])(0?[1-9]|1[1-2])\1\d{4}$',fechanac)
+            fechapc=f.get('Fecha primera compra')
+            fpc=re.fullmatch('^(?:3[01]|[12][0-9]|0?[1-9])([\/])(0?[1-9]|1[1-2])\1\d{4}$',fechapc)
+            
         url=endpoint.format('/datos')
-        requests.post(url,data)
+        requests.post(url,nom)
         return redirect('index')
