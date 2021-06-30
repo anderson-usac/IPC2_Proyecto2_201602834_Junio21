fb49bf1c1a12c93c65d52c3fd0799f697bc6ac38
anderson-usac <2976112160101@ingenieria.usac.edu.gt>
2021-06-30 03:15:48 -0600

    regex corregido

--- frontend/web/views.py
+++ frontend/web/views.py
@@ -7,7 +7,13 @@ from xml.etree import ElementTree as ET
 from xml.dom import minidom
 
 from requests.models import encode_multipart_formdata, parse_url
+from werkzeug.wrappers import response
 
+listaNombre=[]
+listaApellido=[]
+listaEdad=[]
+listaFechacump=[]
+listaFechaprimera=[]
 endpoint= 'http://localhost:5000/{}'
 # Create your views here.
 def index(request):
@@ -18,23 +24,30 @@ def index(request):
         return render(request,'index.html',context)
     elif request.method=='POST':
         doccsv = request.FILES['document']
-        decoded_file = doccsv.read().decode('UTF-8')
-        reader = csv.reader(decoded_file,delimiter=',',skipinitialspace=True)
-        for row in reader:  
-            print(row,end='')
-        
-        #     nombre=f.get('Nombre')
-        #     nom=re.findall('[a-zA-ZáéíóúñÑ]',nombre)
-        #     print(''.join(nom))
-        #     apellidos=f.get('Apellido')
-        #     ape=re.findall('[a-zA-ZáéíóúñÑ]',apellidos)
-        #     edad=f.get('Edad')
-        #     ed=re.findall('[0-9]',edad)
-        #     fechanac=f.get('Fecha de cumpleaños')
-        #     fnac=re.findall('^([0-9]{2}/)([0-1][0-9]{1}/)([0-9]{4})$',fechanac)
-        #     fechapc=f.get('Fecha primera compra')
-        #     fpc=re.findall('^([0-9]{2}/)([0-1][0-9]{1}/)([0-9]{4})$',fechapc)
-            
+        filecsv = doccsv.name
+        with open(filecsv,newline='',encoding='utf-8')as f:
+            datascsv=csv.DictReader(f,delimiter=',',skipinitialspace=True)
+            for f in datascsv:
+                nombre=f.get('Nombre')
+                nom=re.findall('[a-zA-ZáéíóúñÑ]',nombre)
+                listaNombre.append(''.join(nom))
+                apellidos=f.get('Apellido')
+                ape=re.findall('[a-zA-ZáéíóúñÑ]',apellidos)
+                listaApellido.append(''.join(ape))
+                edad=f.get('Edad')
+                ed=re.findall('[0-9]',edad)
+                listaEdad.append(''.join(ed))
+                fechanac=f.get('Fecha de cumpleaños')
+                fnac=re.findall('^([0-9]{2}/)([0-1][0-9]{1}/)([0-9]{4})$',str(fechanac))
+                listaFechacump.append(fnac)
+                fechapc=f.get('Fecha primera compra')
+                fpc=re.findall('^([0-9]{2}/)([0-1][0-9]{1}/)([0-9]{4})$',str(fechapc))
+                listaFechaprimera.append(fpc)
+        print('Nom:',listaNombre)
+        print('Ape:',listaApellido)
+        print('Edad:',listaEdad)
+        print('fecha1_',listaFechacump)
+        print('fecha2:',listaFechaprimera)    
         url=endpoint.format('/datos')
-        requests.post(url,decoded_file)
+        requests.post(url,filecsv)
         return redirect('index')
